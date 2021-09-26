9ff6653a23af16c7fccda630138b5749bc590d31
tomme10 <65664310+tomme10@users.noreply.github.com>
2021-09-26 17:42:38 +0100

    Update raycasting.py

--- raycasting.py
+++ raycasting.py
@@ -4,19 +4,22 @@ from math import sin,cos,sqrt,radians,degrees
 
 
 
-dis = (1920,1080)
+dis = (1920,1080) # define window size
 root = pygame.display.set_mode(dis,pygame.FULLSCREEN)
-cx = dis[0]//2
-cy = dis[1]//2
+cx = dis[0]//2 
+cy = dis[1]//2 
 
 clock = pygame.time.Clock()
 FPS = 0
 
-maxLen = 3000
+maxLen = 3000 # max length of rays
 
 pygame.mouse.set_visible(False)
 
-class circle():
+class circle()
+    
+    # circle hitbox, contains a simple distance estimator and draw methods
+
     def __init__(self,x,y,r):
         self.pos = [x,y]
         self.r = r
@@ -29,6 +32,9 @@ class circle():
         return abs(dist-self.r)
 
 class rectangle():
+    
+    # rectangle hitbox, contains a distance estimator and draw methods
+    
     def __init__(self,x,y,w,h):
         self.pos = [x,y]
         self.wh = [w,h]
@@ -57,30 +63,33 @@ class rectangle():
         
 
 class ray():
+    
+    # The ray class, the ray calculation happens at object creation
+    
     def __init__(self,x,y,angle,colliders):
         self.start = [x,y]
         self.target = [x,y]
 
-        vx,vy = sin(angle),cos(angle)
+        vx,vy = sin(angle),cos(angle) # calculate a movement vector of 1px length in ray direction
 
         length = 0
         step = maxLen
 
         self.circles = []
         
-        while length < maxLen and step > 1:
+        while length < maxLen and step > 1: # move the ray forward aslong as the length is less than maximum length and the last step length was greater than 1
 
             steps = []
             for collider in colliders:
-                steps.append(collider.distEst(self.target))
-            step = min(min(steps),maxLen-length)
+                steps.append(collider.distEst(self.target)) # this gets the value of all distance estimators from the ray
+            step = min(min(steps),maxLen-length) # it takes the minimum of the distance estimators, and then compares it to the maximum length to make sure the ray doesn't go too far
 
             self.circles.append([self.target.copy(),step])
 
-            self.target[0] += vx*step
+            self.target[0] += vx*step # move the end of the ray by the distance calculated
             self.target[1] += vy*step
 
-            length += step
+            length += step # sum the length
 
     def draw(self,root):
         pygame.draw.line(root,(255,0,0),self.start,self.target)
@@ -89,7 +98,7 @@ class ray():
          #   pygame.draw.circle(root,(255,0,255),circle[0],circle[1],1)
             
 
-scene = [circle(dis[0]//2,dis[1]//2,100),rectangle(0,0,1920,50)]
+scene = [circle(dis[0]//2,dis[1]//2,100),rectangle(0,0,1920,50)] # defines the objects in the scene
 
 
 def main(args):
@@ -110,19 +119,19 @@ def main(args):
         rays = []
 
         for i in range(360):
-            rays.append(ray(mx,my,radians(i/8)+frame,scene))
+            rays.append(ray(mx,my,radians(i/8)+frame,scene)) # generate the rays around the mouse cursor
 
         root.fill((0,0,0))
 
         pygame.draw.circle(root,(255,0,0),[mx,my],5)
 
         for obj in scene:
-            obj.draw(root)
+            obj.draw(root) # draw the scene
 
         for r in rays:
-            r.draw(root)
+            r.draw(root) # draw the rays
 
-        pygame.display.update()
+        pygame.display.update() # update the window
         clock.tick(FPS)
 
 if __name__ == '__main__':
